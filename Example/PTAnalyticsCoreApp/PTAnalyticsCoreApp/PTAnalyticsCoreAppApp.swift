07abe1b222db5ca3299bb8c0d3f7a78d87c460d5
happyo <happyojones@gmail.com>
2024-06-05 22:40:45 +0800

    @dev # Init

---
+++ Example/PTAnalyticsCoreApp/PTAnalyticsCoreApp/PTAnalyticsCoreAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PTAnalyticsCoreAppApp.swift
+//  PTAnalyticsCoreApp
+//
+//  Created by happyo on 2024/6/5.
+//
+
+import SwiftUI
+
+@main
+struct PTAnalyticsCoreAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
