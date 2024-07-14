53830fd03184b09094be480bee50a48be4e69eed
happyo <happyojones@gmail.com>
2024-07-14 13:04:38 +0800

    @dev # Update to main actor

--- Example/PTAnalyticsCoreApp/PTAnalyticsCoreApp/PTAnalyticsCoreAppApp.swift
+++ Example/PTAnalyticsCoreApp/PTAnalyticsCoreApp/PTAnalyticsCoreAppApp.swift
@@ -6,12 +6,30 @@
 //
 
 import SwiftUI
+import PTAnalyticsCore
 
 @main
 struct PTAnalyticsCoreAppApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onAppear {
+                    LogEventManager.shared.configure(logger: TestEventLogger())
+                    LogEventManager.logTest(prefix: "123")
+                }
         }
+    }
+}
+
+class TestEventLogger: EventLogger {
+    func logEvent(name: String, params: [String : Any]?) {
+        print(name)
+    }
+}
+
+@MainActor
+extension LogEventManager {
+    public static func logTest(prefix: String) {
+        shared.logEvent(name: prefix)
     }
 }
--- Sources/PTAnalyticsCore/LogEventManager.swift
+++ Sources/PTAnalyticsCore/LogEventManager.swift
@@ -11,14 +11,17 @@ public actor LogEventManager {
     @MainActor
     public static let shared = LogEventManager()
 
+    @MainActor
     private var eventLogger: EventLogger?
 
     private init() {}
 
+    @MainActor
     public func configure(logger: EventLogger) {
         self.eventLogger = logger
     }
 
+    @MainActor
     public func logEvent(name: String, params: [String: Any]? = nil) {
         eventLogger?.logEvent(name: name, params: params)
     }
